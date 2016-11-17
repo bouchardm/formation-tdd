e39cbc4d1748c7b0bbdf30608023e4ab07292522
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-11-16 19:56:25 -0500

    #2 Setup before

--- Tests/FizzBuzzTest.php
+++ Tests/FizzBuzzTest.php
@@ -6,5 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    
+    public function __construct()
+    {
+        $this->fb = new FizzBuzz();
+    }
+
+    public function test()
+    {
+
+    }
 }
